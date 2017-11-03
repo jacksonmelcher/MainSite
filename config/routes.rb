72564d9e6c1b5184a9f0bc57f42c2d2308aa2bef
Austin <austin@dringo.io>
2017-11-03 14:59:47 -0700

    refactor: Changed how the code was structured for sign in and default root paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
-
+  ############################################################################
+  # USERS
+  ############################################################################
   devise_for :users
+  resources :users, only: [:show, :index]
+
+
+  ############################################################################
+  # LANDING
+  ############################################################################
   get 'welcome/index'
 
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  authenticated :user do
+    root to: "users#index"
+  end
+
+  unauthenticated :user do
+    root 'welcome#index'
+  end
 end
