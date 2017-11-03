f6af2d5cb55b905df8bde573c24920d2ac81fa13
Austin <austin@dringo.io>
2017-11-03 13:39:09 -0700

    feature: Have sign in process mapped out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
-      concat(content_tag(:div, message, role: "alert", class: "alert #{bootstrap_class_for(msg_type)} alert-dismissible fade in") do
+      concat(content_tag(:div, message, role: "alert", class: "alert alert-dismissible fade in") do
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' }, aria: { label: 'Close' })
               concat message
             end)
